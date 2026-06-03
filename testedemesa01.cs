a18166735a8d1893e36e22d63bed171fcb6e13c6
Nicoli <nicolisilva@bne.com.br>
2026-06-03 15:13:58 -0300

    Add files via upload

---
+++ testedemesa01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Tabela1
+{
+    static void Main()
+    {
+        const int a = 10;
+        const int b = 20;
+        int c;
+
+        c = (a + b) / 2;
+        c = c - 40;
+
+        int[] v = new int[4];
+        v[3] = a + b + c;
+
+        Console.WriteLine(v[3]);
+    }
+} 
+
+
+
+
+
+
