35d6018b22566f8a30fe4081c21c1e61dc4c08fb
Nicoli <nicolisilva@bne.com.br>
2026-06-03 15:46:15 -0300

    Add files via upload

---
+++ TESTEDEMESA02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Tabela2
+{
+    static void Main()
+    {
+        int a = 2;
+        int[] v = new int[7];
+
+        while (a < 6)
+        {
+            v[a] = 10 * a;
+            a++;
+        }
+
+        for (int i = 2; i < 6; i++)
+        {
+            Console.WriteLine($"v[{i}] = {v[i]}");
+        }
+
+        Console.ReadKey();
+    }
+}
